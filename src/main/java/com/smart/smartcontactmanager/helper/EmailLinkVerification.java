35df09c7105e10c9f7924849dd3ffa2b2f95f665
PushkalSharma0907 <devvratsharma0907@gmail.com>
2026-04-13 18:05:02 +0530

    config

--- src/main/java/com/smart/smartcontactmanager/helper/EmailLinkVerification.java
+++ src/main/java/com/smart/smartcontactmanager/helper/EmailLinkVerification.java
@@ -1,11 +1,13 @@
 package com.smart.smartcontactmanager.helper;
 
 
+
 public class EmailLinkVerification {
 	
 	
 	public static String generateVerificationLink(String emailToken) {
 		
+			
 		String verificationLink = "http://localhost:8080/auth/verify-email?token=" + emailToken;
 
 		return verificationLink;
