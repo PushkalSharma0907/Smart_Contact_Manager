a9fdc43cda8d6705e540573361fb4644efda3377
PushkalSharma0907 <devvratsharma0907@gmail.com>
2026-04-14 01:58:37 +0530

    message

--- src/main/java/com/smart/smartcontactmanager/helper/Message.java
+++ src/main/java/com/smart/smartcontactmanager/helper/Message.java
@@ -17,32 +17,5 @@ public class Message {
     
     @Builder.Default
     private MessageType type = MessageType.blue;
-
-	public Message(String content, MessageType type) {
-		super();
-		this.content = content;
-		this.type = type;
-	}
-    
-	// ✅ Getter for content
-    public String getContent() {
-        return content;
-    }
-
-    // ✅ Setter for content (optional)
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    // ✅ Getter for type
-    public MessageType getType() {
-        return type;
-    }
-
-    // ✅ Setter for type (optional)
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-
     
 }
